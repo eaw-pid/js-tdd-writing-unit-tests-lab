5cdfd30cf8e21d9bf6c0cba931fd0a1a1938f4e0
Erin Wise <erinwise88@gmail.com>
2023-10-24 21:41:01 -0400

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,20 @@
 // Your tests here
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    it("checks if a word is the same backwards as forwards", () => {
+        const word = "racecar"
+        const result = isPalindrome(word)
+        expect(result).toBe(true)
+    })
+});
+    it("checks if a word is the same backwards as forwards", () => {
+    const word = "car"
+    const result = isPalindrome(word)
+    expect(result).toBe(false)
+})
+    it("checks if a word is the same backwards as forwards", () => {
+    const word = "A man, a plan, a canal. Panama"
+    const result = isPalindrome(word)
+    expect(result).toBe(true)
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,22 @@
 // Your code here
+export function isPalindrome(word) {
+  const re=/[\W_]/g
+
+  const lowWord = word.toLowerCase().replace(re, "")
+
+  const reverseWord = lowWord.split("").reverse().join("");
+
+  return reverseWord === lowWord
+  
+    // let j = word.length - 1;
+    // for (let i = 0; i < j /2; i++) {
+    //     let x = word[i];
+    //     let y = word[j - i];
+    //     if (x != y) {
+    //         return false
+    //     }
+    // }
+    // return true
+}
+
+
